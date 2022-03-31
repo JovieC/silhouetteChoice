8740360f47aa686db18a8e9589ebd890a6c22ef8
JovieC <qiongwec@usc.edu>
2022-03-31 12:11:00 -0500

    no space

--- silhouette_choiceTask_main.js
+++ silhouette_choiceTask_main.js
@@ -1,14 +1,16 @@
+var STIM_N = 120; // number of trials in choice task
+
 var timeline = [];
 
-var pic = pic_list.map(i => "https://joviec.github.io/silhouetteTask/img/"+ i.name);
+// var pic = pic_list.map(x => "https://joviec.github.io/silhouetteTask/img/" + x.name);
 
-var preload = {
-     type:'preload',
-     auto_preload:true,
-     images: pic
- };
+// var preload = {
+//      type:'preload',
+//      auto_preload:true,
+//      images: pic
+//  };
 
-timeline.push(preload);
+// timeline.push(preload);
 
 var welcome = {
             type: "html-keyboard-response",
@@ -28,26 +30,25 @@ timeline.push(welcome);
                     trial_duration: 500,
                     response_ends_trial: false
                 };
-                //timeline.push(fixation);
+                timeline.push(fixation);
 
                 var trial = {
                     type: 'html-button-response',
-                    stimulus: function () {
-                        return (
-                            '<div class="issue"><img src="https://joviec.github.io/silhouetteTask/img/'+jsPsych.timelineVariable("name", true)
-                            );
-                        },
+                    stimulus: '<img src=https://joviec.github.io/silhouetteTask/img/finalPics FemaleG1Age0.2P2FemaleG6Age67P1MaleG4Age14P2MaleG2Age2P2.png'
+                    //function () {
+                      //  return '<img src="https://joviec.github.io/silhouetteTask/img/finalPics FemaleG1Age0.2P2FemaleG6Age67P1MaleG4Age14P2MaleG2Age2P2.png'//+jsPsych.timelineVariable("name", true)+'">'
+                        //},
                     choices: ['1', '2', '3', '4'],
                     //data: values
                         
                 };
 
-                //timeline.push(trial);
+                timeline.push(trial);
                 //}
 
             var trials_with_variables1 = {
                 timeline: [fixation, trial],
-                timeline_variables: stim_list.slice(0,STIM_N/2)
+                timeline_variables: pic_list//.slice(0,STIM_N/2)
             };
 
             timeline.push(trials_with_variables1);
